05ce96e9e24c0bd4b5e8f6faefaec467ac7312d5
AramWondergem <aramwondergem@gmail.com>
2022-11-07 15:46:47 +0100

    finished the assignment

--- scr/main.js
+++ scr/main.js
@@ -4,28 +4,11 @@
 // Je moet gebruik maken van de eerste parameter!
 
 function pepernotenFabriek(string,numberOfPepernoten) {
-    // pepernoten  notpepener
 
-    const firstPart = string.slice(3,6);
-    const secondPart = string.slice(8,);
-    const thirdPart = string.charAt(0);
-    let fourthPart = "";
-    for (let i = 0; i < 2; i++) {
-        fourthPart += string.charAt(1)
-    }
-    const fifthPart = string.charAt(2);
 
-    const onverpaktePepernoot = firstPart + secondPart + thirdPart + fourthPart +fifthPart;
+    // Voeg hier code toe om de test weer werkend te krijgen! Succes.
 
-    onverpaktePepernoot.toUpperCase();
 
-    const pepernotenZak = new Array();
-
-    for (let i = 0; i < numberOfPepernoten.charCodeAt(0); i++) {
-
-        pepernotenZak.push(onverpaktePepernoot)
-
-    }
 
     pepernotenZak.map((pepernoot) => {
         console.log(pepernoot)
